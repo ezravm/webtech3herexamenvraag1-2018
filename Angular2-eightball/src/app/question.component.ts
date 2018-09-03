dcfdb4e70aa5733efed7957d69bc0398df20b29a
ezravm <33087661+ezravm@users.noreply.github.com>
2018-09-03 16:49:20 +0200

    added service, made eightball working without storage
    added picture, made working eightball, without storage, but using service.

--- Angular2-eightball/src/app/question.component.ts
+++ Angular2-eightball/src/app/question.component.ts
@@ -19,6 +19,7 @@ export class QuestionComponent {
           });
       }
 
+
    constructor(private questionService: QuestionService) {}
 
    onSubmit() {
--- Angular2-eightball/src/app/question.service.ts
+++ Angular2-eightball/src/app/question.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Response } from '@angular/http';
 
 
+
 import {Question} from './question';
 
 @Injectable()
@@ -13,6 +14,7 @@ askQuestion(question: String): String {
 
 var answers = ["it is certain","it is decidedly so","without a doubt", "yes - definitely","you may rely on it", "as i see it, yes", "most likely", "outlook good", "yes", "signs point to yes", "ask again later", "better not tell you now", "cannot predict now", "concentrate and ask again", "my reply is no", "my sources say no", "outlook not so good", "very doubtful", "reply hazy, try again", "dont count on it"];
 var random = Math.floor(Math.random() * 20);;
+
 
 
 
